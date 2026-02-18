4653c01f142c2cfab87cfce2a285d7fb1091e91c
DFDEV18 <inventcolab.nishkarsh@gmail.com>
2026-02-18 16:19:45 +0530

    "homeessentials"

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const baseUrl = 'https://172.16.4.31:9902/api';
+  static const baseUrl = 'https://172.16.4.31:9907/api';
 
   static const manageOutlet = '/ManageOutlet/GetOutletMenusForPOS';
 }
